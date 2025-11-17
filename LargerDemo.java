b61eeb8819fdb972f08ae11a6e9d914cfb16ca8b
Kris4869 <kidkris4869@gmail.com>
2025-11-17 21:13:05 +0800

    Initial commit

---
+++ LargerDemo.java
@@ -0,0 +1,16 @@
+/** Demostrates the creation of a method in Java */
+public class LargerDemo {
+    /** Returns the larger of x and y. */
+    public static int larger(int x,int y){
+        if (x > y){
+            return x;
+        }
+        else{
+            return y;
+        }
+    }
+
+    public static void main(String[] args) {
+         System.out.println(larger(-5, 2));
+    }
+}
